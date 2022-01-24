a3fcadf41c8460b2640adeecd0d449da3d859b5f
Marcus K <marcus-github@te-amor.de>
2022-01-24 14:43:47 +0100

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "message-queue adapter settings": {
+        "en": "Adapter settings for message-queue",
+        "de": "Adaptereinstellungen für message-queue",
+        "ru": "Настройки адаптера для message-queue",
+        "pt": "Configurações do adaptador para message-queue",
+        "nl": "Adapterinstellingen voor message-queue",
+        "fr": "Paramètres d'adaptateur pour message-queue",
+        "it": "Impostazioni dell'adattatore per message-queue",
+        "es": "Ajustes del adaptador para message-queue",
+        "pl": "Ustawienia adaptera dla message-queue",
+        "zh-cn": "message-queue的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "вариант 2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
